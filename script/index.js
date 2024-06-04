9386787ddb889647901dc810c4d0ea6cc419b293
oyintanda-zongwana <oyintandazongone@gmail.com>
2024-06-04 16:39:05 +0200

    done

--- script/index.js
+++ script/index.js
@@ -3,12 +3,12 @@
 function calculateTriangleArea() {
     let base = parseFloat(document.getElementById('Base').value);
     let height = parseFloat(document.getElementById('Height').value);
-
+    // to insure that both base and height are positive numbers
     if (base <= 0 || height <= 0 || isNaN(base) || isNaN(height)) {
         document.getElementById('result').innerHTML = "Base and height must be positive numbers.";
         return;
     }
-
+    //area of triangle formula
     let area = 0.5 * base * height;
-    document.getElementById('result').innerHTML = "The area of the triangle is: " + area;
+    document.getElementById('result').innerHTML = "The area of the triangle is: " + area; //the result
 }
